c20b68eda2f9b8df74321634ed539fdf9c3afa61
zph224 <zph224@users.noreply.github.com>
2022-04-23 18:44:58 +0800

    Update ip.py
    更新开机自启说明

--- ip.py
+++ ip.py
@@ -12,7 +12,7 @@ def getip():
     msg = ("当前IP：" + ip)
     url = 'http://www.pushplus.plus/send?token=27e55baa41434cb9a07b6cfea2696044&content={}&template=markdown'.format(msg)
     reqp = requests.get(url)
-
+#使用sudo nano /etc/rc.local 在exit0之前添加运行即可开机自动发送IP    
 
 if __name__ == "__main__":
     getip()
